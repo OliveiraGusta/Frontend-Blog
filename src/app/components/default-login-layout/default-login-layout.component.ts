09faee835244658dd5268a2443cab52f1f8b5652
OliveiraGusta <gustavo.oliver456@gmail.com>
2025-02-10 23:04:36 -0300

    feat: estrutura incial, com imagens

---
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-login-layout',
+  imports: [],
+  templateUrl: './default-login-layout.component.html',
+  styleUrl: './default-login-layout.component.css'
+})
+
+export class DefaultLoginLayoutComponent {
+
+}
